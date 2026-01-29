3d4c531d2f2ac4f53c04be9ffe5de0ada88e8819
Eugene Shmorgun <eugene.shmorgun@vmedia.ca>
2026-01-29 18:15:28 +0200

    main is cleared.

--- scripts/consume-topics.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Consuming orders_enriched topic (from beginning). Use Ctrl+C to stop."
-docker run --rm --network host bitnami/kafka:3.6 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic orders_enriched --from-beginning
--- scripts/produce-sample-users.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-curl -X POST -H "Content-Type: application/json" http://localhost:8081/users -d '{"userId":"u1","name":"Eugene","status":"active","country":"UA"}'
-curl -X POST -H "Content-Type: application/json" http://localhost:8081/users -d '{"userId":"u2","name":"Anna","status":"active","country":"DE"}'
-echo "\nProduced sample users"
